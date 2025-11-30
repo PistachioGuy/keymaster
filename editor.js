37da27a34ce60f4e54cfae647cd0b54a8111b731
Kiran <kirlubla@gmail.com>
2025-11-29 23:36:25 -0500

    second commit

---
+++ editor.js
@@ -0,0 +1,148 @@
+
+if (localStorage.getItem("secrets")===null) {
+	var secrets = {};
+} else {
+	var secrets =JSON.parse(localStorage.getItem("secrets"));
+}
+var codedisplays = document.getElementById("codedisplays");
+var len = null
+function displaysecrets() {
+	if (Object.keys(secrets).length === 0){
+		codedisplays.textContent="No codes set up yet, add some!";
+		codedisplays.innerHTML+=("<p><button id='service0' onclick='newentry();'>New</button></p>"); //the "New" button is called "service#" # being the amount of services, so it gets selected when the user gets to the bottom
+	} else {
+		len = Object.keys(secrets).length;
+		codedisplays.textContent="";
+		for (var i = 0; i < len; i++) {
+			service=Object.keys(secrets)[i];
+			console.log(service);
+			codedisplays.innerHTML+=("<p><input type=text id='service"+String(i)+"'value='"+service+"'><button id='delete"+String(i)+"' onclick='delentry(\""+String(i)+"\");'>Delete</button></p>");
+			codedisplays.innerHTML+=("<p>Secret:<input type=text onclick='newentry();' id='secret"+String(i)+"'value='"+secrets[service]["secret"]+"'></p>");
+		}
+		codedisplays.innerHTML+=("<p><button id='service"+String(len)+"' onclick='newentry();'>New</button></p>"); //the "New" button is called "service#" # being the amount of services, so it gets selected when the user gets to the bottom
+	}
+}
+displaysecrets();
+document.getElementById("service0").focus();
+var selectedType = "service";
+var selectedServiceId=0;
+function save() {
+	console.log(secrets);
+	len = Object.keys(secrets).length;
+	secrets = {};
+	for (var i = 0; i<len; i++) {
+		secrets[document.getElementById("service"+i).value] = {"secret":(document.getElementById("secret"+i).value)} 
+		console.log(document.getElementById("service"+i).value)
+	}
+	console.log(secrets);
+	localStorage.setItem("secrets", JSON.stringify(secrets));
+	window.location.href = "index.html";
+}
+
+function newentry() {
+	len = Object.keys(secrets).length;
+	secrets = {};
+	for (var i = 0; i<len; i++) {
+		secrets[document.getElementById("service"+i).value] = {"secret":(document.getElementById("secret"+i).value)} 
+		console.log(document.getElementById("service"+i).value)
+	}
+	selectedType = "service";
+	selectedServiceId = len;
+	var newentryid=len+1;
+	secrets["Entry "+newentryid] = {"secret":""};
+	displaysecrets();
+	document.getElementById(selectedType+selectedServiceId).focus();
+}
+function delentry(entry) {
+	if (confirm("Delete entry "+(entry+1)+"?") === true) {
+		delete secrets[Object.keys(secrets)[entry]]
+	}
+	displaysecrets();
+}
+function moveup() {
+	var el=null;
+	deleteselected=false;
+	if (selectedType==="secret") {
+		el=document.getElementById("delete"+selectedServiceId)
+		if (el) {
+			console.log(el);
+			el.focus();
+			selectedType="delete";
+			console.log("delete"+selectedServiceId);
+		}
+	}else if (selectedType==="delete") {
+		el=document.getElementById("service"+selectedServiceId)
+		if (el) {
+			console.log(el);
+			el.focus();
+			selectedType="service";
+			console.log("service"+selectedServiceId);
+		}
+	} else {
+		if (selectedServiceId === 0) {
+			selectedType = "savebtn";
+			console.log(document.getElementById("savebtn"));
+			document.getElementById("savebtn").focus();
+		} else{
+			el = document.getElementById("secret"+(selectedServiceId-1))
+			if (el) {
+				console.log(el);
+				el.focus();
+				selectedServiceId--;
+				selectedType="secret";
+				console.log("secret"+selectedServiceId);
+			}
+		}
+	}
+}
+
+function movedown() {
+	var el=null;
+	deleteselected=false;
+	if (selectedType === "savebtn") {
+		selectedType = "service";
+		selectedServiceId = 0;
+		document.getElementById("service"+selectedServiceId).focus();
+	} else {
+		if (selectedType==="service") {
+			el=document.getElementById("delete"+selectedServiceId)
+			if (el) {
+				el.focus();
+				console.log("delete"+selectedServiceId);
+				selectedType="delete";
+			}
+		} else if (selectedType==="delete") {
+			el = document.getElementById("secret"+selectedServiceId)
+			if (el) {
+				el.focus();
+				console.log("secret"+selectedServiceId);
+				selectedType="secret";
+			}
+		}
+		else {
+			el = document.getElementById("service"+(selectedServiceId+1))
+			if (el) {
+				el.focus();
+				selectedServiceId++;
+				console.log("service"+selectedServiceId);
+				selectedType="service";
+			}
+		}
+	}
+}
+
+window.addEventListener("load", () => {
+
+document.addEventListener("keydown", evt => {
+    console.log("Pressed:", evt.key);
+
+    if (evt.key === "ArrowUp") {
+        moveup();
+    }
+	if (evt.key === "ArrowDown") {
+        movedown();
+    }
+	
+});
+
+});
---
+++ main.js
@@ -0,0 +1,70 @@
+// Alternative:
+// const secret = authenticator.generateSecret();
+// Note: .generateSecret() is only available for authenticator and not totp/hotp
+var totp = new jsOTP.totp();
+var secrets =JSON.parse(localStorage.getItem("secrets"));
+var len = Object.keys(secrets).length;
+var errorout = null;
+function displaysecrets() {
+	if (Object.keys(secrets).length === 0){
+		codedisplays.textContent="No accounts set up yet, add some in settings!";
+	} else {
+		len = Object.keys(secrets).length;
+		codedisplays.textContent="";
+		for (var i = 0; i < len; i++) {
+			service=Object.keys(secrets)[i];
+			console.log(service);
+			codedisplays.innerHTML+=("<p id='service"+String(i)+"'>"+service+":</p>");
+			codedisplays.innerHTML+=("<b id='secret"+String(i)+"'>"+secrets[service]+"</b>");
+		}
+	}
+}
+displaysecrets();
+
+var endtime=60;
+function generateAllCodes() {
+	for (var i = 0; i < len; i++) {
+		service=Object.keys(secrets)[i];
+		console.log(service);
+		console.log(secrets[service].secret);
+		try{
+			timeCode = totp.getOtp(secrets[service].secret);
+			document.getElementById("secret"+i).textContent = timeCode;
+		} catch (errorout) {
+			document.getElementById("secret"+i).textContent = "Error, is the secret correct?";
+		}
+	}
+}
+generateAllCodes();
+function loop() {
+	var timeleft=document.getElementById("timer");
+	len = Object.keys(secrets).length;
+	
+	var seconds = new Date().getSeconds();
+	if (endtime === 60 && seconds < 30 && seconds > 0) {
+		endtime = 30;
+		generateAllCodes();
+		console.log("generated");
+	} else if (endtime === 30 && seconds < 60 && seconds > 30) {
+		endtime = 60;
+		generateAllCodes();
+		console.log("generated");
+	} 
+	timeleft.textContent="Time left: "+(endtime-seconds)
+	setTimeout(loop, 1000);
+}
+loop();
+
+window.addEventListener("load", () => {
+
+document.addEventListener("keydown", evt => {
+    console.log("Pressed:", evt.key);
+
+    if (evt.key === "SoftLeft") {
+		window.location.href = "editor.html";
+    }
+});
+
+});
+	
+	
